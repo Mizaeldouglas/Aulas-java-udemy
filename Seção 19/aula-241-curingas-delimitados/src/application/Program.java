72a120ddaebe09dd430b51690598e80ea0e1201d
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-08-14 19:10:41 -0300

    aula 241 Curingas delimitados

--- Seção 19/aula-241-curingas-delimitados/src/application/Program.java
+++ Seção 19/aula-241-curingas-delimitados/src/application/Program.java
@@ -1,7 +1,32 @@
 package application;
 
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 	public static void main(String[] args) {
 
+		List<Shape> myShape = new ArrayList<>();
+		myShape.add(new Rectangle(3.0,2.0));
+		myShape.add(new Circle(2.0));
+
+		List<Circle> myCicle = new ArrayList<>();
+		myCicle.add(new Circle(2.0));
+		myCicle.add(new Circle(3.0));
+
+		System.out.println("Total ara: " + totalArea(myCicle));
+
+	}
+	public static double totalArea(List<? extends Shape> list){
+		double sum = 0.0;
+		for (Shape s :
+				list) {
+			sum += s.area();
+		}
+		return sum;
 	}
 }
--- Seção 19/aula-241-curingas-delimitados/src/entities/Shape.java
+++ Seção 19/aula-241-curingas-delimitados/src/entities/Shape.java
@@ -1,4 +1,5 @@
 package entities;
 
-public class Shape {
+public interface Shape {
+	double area();
 }
